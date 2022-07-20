ceececcf861e8a6004866d608fb57aa2cfad31bb
Shahid Ameen <43674745+iamshahidameen@users.noreply.github.com>
2022-07-21 03:03:45 +0500

    Added a layout for Resume App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
-import React, { useState, useEffect } from 'react'
-import { FaAngleDoubleRight } from 'react-icons/fa'
+import React, { useState, useEffect } from 'react';
+import { FaAngleDoubleRight } from 'react-icons/fa';
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tabs-project'
+const url = 'https://course-api.com/react-tabs-project';
+
 function App() {
-  return <h2>tabs project setup</h2>
+  const [details, setDetails] = useState([]);
+  useEffect(() => {
+    const userData = fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => setDetails(data));
+  }, []);
+  return (
+    <>
+      <section className="section">
+        <div className="title">
+          <h2>experience</h2>
+          <div className="underline"></div>
+        </div>
+        <div className="jobs-center">
+          <div className="btn-container">
+            <button className="job-btn active-btn">TOMMY</button>
+          </div>
+          <article className="job-info">
+            <h3>Full Stack Web Developer</h3>
+            <h4>TOMMY</h4>
+            <p className="job-date">December 2015 - Present</p>
+            <div className="job-desc">
+              <FaAngleDoubleRight className="job-icon" />
+              <p>
+                Tote bag sartorial mlkshk air plant vinyl banjo lumbersexual
+                poke leggings offal cold-pressed brunch neutra. Hammock photo
+                booth live-edge disrupt.
+              </p>
+            </div>
+          </article>
+        </div>
+        <button type="button" className="btn">
+          more info
+        </button>
+      </section>
+    </>
+  );
 }
 
-export default App
+export default App;
