2db80ebd792bf7225b40721328e06d67ab425a03
Shahid Ameen <43674745+iamshahidameen@users.noreply.github.com>
2022-07-21 22:24:24 +0500

    Added tabs functionality with tabs being duplicated bug

--- src/App.js
+++ src/App.js
@@ -5,12 +5,23 @@ import { FaAngleDoubleRight } from 'react-icons/fa';
 const url = 'https://course-api.com/react-tabs-project';
 
 function App() {
+  const [loading, setLoading] = useState(true);
   const [jobs, setJobs] = useState([]);
   const [selectedJob, setSelectedJob] = useState([]);
+
+  // fetch(url)
+  //   .then((resp) => resp.json())
+  //   .then((data) => setJobs(data));
+
+  async function getApiData() {
+    const resp = await fetch(url);
+    const data = await resp.json();
+    setJobs(data);
+    setSelectedJob(data[0]);
+    setLoading(false);
+  }
   useEffect(() => {
-    fetch(url)
-      .then((resp) => resp.json())
-      .then((data) => setJobs(data));
+    getApiData();
   }, []);
   console.log(jobs, 'all jobs');
 
@@ -19,53 +30,66 @@ function App() {
 
   function filterJob(clickedJob) {
     const newJob = jobs.filter((job) => job.company === clickedJob);
-    setSelectedJob(newJob);
+    setSelectedJob(newJob[0], 'this is new job');
+    console.log(newJob[0], 'this is new job');
   }
-
-  return (
-    <>
-      <section className="section">
-        <div className="title">
-          <h2>experience</h2>
-          <div className="underline"></div>
-        </div>
-        <div className="jobs-center">
-          <div className="btn-container">
-            {jobs.map((job, index) => {
-              //console.log(job);
-
-              return (
-                <button
-                  key={index}
-                  className="job-btn"
-                  onClick={() => filterJob(job.company)}
-                >
-                  {job.company}
-                </button>
-              );
-            })}
+  const { id, company, dates, title } = selectedJob;
+  // setJobDuties(selectedJob.duties);
+  // console.log(jobDuties, 'job duties');
+  if (loading) {
+    return <h2>Content Is Being loaded</h2>;
+  }
+  if (jobs) {
+    return (
+      <>
+        <section className="section">
+          <div className="title">
+            <h2>experience</h2>
+            <div className="underline"></div>
           </div>
-          <article className="job-info">
-            <h3>Full Stack Web Developer</h3>
-            <h4>TOMMY</h4>
-            <p className="job-date">December 2015 - Present</p>
-            {/* {selectedJob ? console.log(selectedJob.title, 'yessss') : ''} */}
-            <div className="job-desc">
-              <FaAngleDoubleRight className="job-icon" />
-              <p>
-                Tote bag sartorial mlkshk air plant vinyl banjo lumbersexual
-                poke leggings offal cold-pressed brunch neutra. Hammock photo
-                booth live-edge disrupt.
-              </p>
+          <div className="jobs-center">
+            <div className="btn-container">
+              {jobs.map((job, index) => {
+                console.log(job, 'new console check');
+
+                return (
+                  <button
+                    key={id}
+                    className="job-btn"
+                    onClick={() => filterJob(job.company)}
+                  >
+                    {job.company}
+                  </button>
+                );
+              })}
             </div>
-          </article>
-        </div>
-        <button type="button" className="btn">
-          more info
-        </button>
-      </section>
-    </>
-  );
+            {
+              <article className="job-info">
+                <h3>{title}</h3>
+                <h4>{company}</h4>
+                <p className="job-date">{dates}</p>
+                {selectedJob.duties.map((duty) => {
+                  return (
+                    <div className="job-desc">
+                      <FaAngleDoubleRight className="job-icon" />
+                      <p>
+                        Tote bag sartorial mlkshk air plant vinyl banjo
+                        lumbersexual poke leggings offal cold-pressed brunch
+                        neutra. Hammock photo booth live-edge disrupt.
+                      </p>
+                    </div>
+                  );
+                })}
+              </article>
+            }
+          </div>
+          <button type="button" className="btn">
+            more info
+          </button>
+        </section>
+      </>
+    );
+  }
 }
 
 export default App;
